0c3c033b0d03aae58b75e6ea01cdc029cea6ced0
Dustin Garrett <dustin.garrett@stonecroptech.com>
2019-08-02 23:53:29 -0500

    Home Automation
    Added bootstrap 4 and styled page. added testing for site layout
    and new pages. Starting user framwork.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
-
-  get  'static_pages/home'
-
-  get  'static_pages/help'
-
-  get  'static_pages/contact'
-
-end
+    get  '/help',    to: 'static_pages#help'
+    get  '/contact', to: 'static_pages#contact'
+    get  '/signup',  to: 'users#new'
+  end
