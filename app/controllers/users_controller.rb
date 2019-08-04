ae143796bbad831231d9b74e47ad682dbfc32b32
Dustin Garrett <dustin.garrett@stonecroptech.com>
2019-08-03 22:25:05 -0500

    Created user login and formated user show.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,9 @@ class UsersController < ApplicationController
   end
   def create
     @user = User.new(user_params)
+    user_full_name = (@user.fname+" "+@user.lname)
     if @user.save
+      flash[:success] = "User "+user_full_name+" Created"
       redirect_to @user
     else
       render 'new'
@@ -19,7 +21,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password,
+      params.require(:user).permit(:fname, :lname, :email, :role, :password,
                                    :password_confirmation)
     end
 end
